2353655f1be64f51780d55b1929a39964e48ba8f
kscherle <kscherle@stud.hs-heilbronn.de>
2022-04-03 13:10:27 +0200

    new 5x12 image

--- place_hhn.py
+++ place_hhn.py
@@ -4,18 +4,19 @@ from PIL import Image
 
 
 
-if len(sys.argv) <= 4:
-    print("usage: python3 place_tile.py [reddit username] [reddit password] [X] [y]")
+if len(sys.argv) <= 5:
+    print("usage: python3 place_tile.py [reddit username] [reddit password] [X] [y] [filename]")
     exit()
 
 username = sys.argv[1]
 password = sys.argv[2]
 X = int(sys.argv[3])
 Y = int(sys.argv[4])
+filename = sys.argv[5]
 
 placer = Placer()
 placer.login(username, password)
 
 
-placer.maintain_image(X, Y, 'HHN-3x10.png', (3, 10))
+placer.maintain_image(X, Y, filename, (3, 10))
 
