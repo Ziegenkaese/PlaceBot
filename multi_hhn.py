7ceaf319e0a976ac4c44b5bbb9042bcb99777132
Ziegenkaese <102874979+Ziegenkaese@users.noreply.github.com>
2022-04-04 20:48:47 +0200

    big update

---
+++ multi_hhn.py
@@ -0,0 +1,22 @@
+import json
+import sys
+from PIL import Image
+from place_bot import Placer, Color
+
+placer = Placer()
+X = int(sys.argv[3])
+Y = int(sys.argv[4])
+filename = sys.argv[5]
+
+img = Image.open(filename)
+W = img.width
+H = img.height
+img.close()
+
+users = json.load(open("users.json", "r"))
+for user in users:
+    print("logging in:",user["username"])
+    placer.login(user["username"], user["password"])
+
+
+placer.maintain_image(X, Y, filename, (H, W))
